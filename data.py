adf20d43604a356109b0a24c51244cfb2dc5d379
Miha Marinko <miha.marinko20@gmail.com>
2019-03-17 10:31:37 +0100

    more music

--- data.py
+++ data.py
@@ -51,6 +51,90 @@ MUSIC_URLS = [
         '\nMusic provided by NoCopyrightSounds.\n'
         'Watch: https://youtu.be/tcHJodG5hX8\n'
         'Free Download / Stream: http://ncs.io/WhyDoIYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=oW3GwO3VCVo',
+        'Song: John Kenza - Wicked [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Video Link: https://youtu.be/oW3GwO3VCVo\n'
+        'Download/Stream: http://ncs.io/Wicked'
+    ),
+    (
+        'https://www.youtube.com/watch?v=-cWkoBJuu-k',
+        'Track: CØDE - Duck Face [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: https://youtu.be/-cWkoBJuu-k\n'
+        'Free Download / Stream: http://ncs.io/DuckFaceYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=yHLtE1wFeRQ',
+        'Track: Jim Yosef & Anna Yvette - Linked [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: https://youtu.be/yHLtE1wFeRQ\n'
+        'Free Download / Stream: http://ncs.io/2017Linked'
+    ),
+    (
+        'https://www.youtube.com/watch?v=WtaKxxZGCKc',
+        'Track: RetroVision - Puzzle VIP [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        '\nWatch: https://youtu.be/WtaKxxZGCKc'
+        'Free Download / Stream: http://ncs.io/PuzzleVIPYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=KPdPJr20loE',
+        'Track: ElementD & Chordinatez - Radiate (feat. Mees Van Den Berg) [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: https://youtu.be/KPdPJr20loE\n'
+        'Free Download / Stream: http://ncs.io/RadiateYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=g04nYW6Y7wE',
+        'Track: Disfigure - Blank (HYLO Remix) [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: \n'
+        'Free Download / Stream: http://ncs.io/BlankRemixYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=ZLohS_HScwc',
+        'Track: 3rd Prototype - Dancefloor [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: https://youtu.be/ZLohS_HScwc\n'
+        'Free Download / Stream: http://ncs.io/DancefloorYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=V-mP3VU0DCg',
+        'Track: NIVIRO - Flares [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: https://youtu.be/V-mP3VU0DCg\n'
+        'Free Download / Stream: http://ncs.io/FlaresYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=LJeiQw2RmSg',
+        'Track: Kisma - Fingertips [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: https://youtu.be/LJeiQw2RmSg\n'
+        'Free Download / Stream: http://ncs.io/FingertipsYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=XVNPdmwOfKI',
+        'Track: Emdi x Coorby - Lonewolf (feat. Kristi-Leah) [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: https://youtu.be/XVNPdmwOfKI\n'
+        'Free Download / Stream: http://ncs.io/LonewolfYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=gDwVGS75sUA',
+        'Track: Lemon Fight - Stronger (feat. Jessica Reynoso) [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: https://youtu.be/gDwVGS75sUA\n'
+        'Free Download / Stream: http://ncs.io/LFstrongerYO'
+    ),
+    (
+        'https://www.youtube.com/watch?v=mIbJpx16-Og',
+        'Track: Weero - Mates [NCS Release]'
+        '\nMusic provided by NoCopyrightSounds.\n'
+        'Watch: https://youtu.be/mIbJpx16-Og\n'
+        'Free Download / Stream: http://ncs.io/MatesYO'
     )
 ]
 
